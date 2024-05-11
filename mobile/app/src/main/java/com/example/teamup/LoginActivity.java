3f57bc18dbb0d58434e54baf6dfa6eeaf08d9bcd
Bruno Rocha <brunorocha2674@gmail.com>
2024-05-11 20:39:50 -0300

    feat(app): add oauth view

--- mobile/app/src/main/java/com/example/teamup/LoginActivity.java
+++ mobile/app/src/main/java/com/example/teamup/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.teamup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.EventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView chooseLoginMethod = findViewById(R.id.chooseLoginMethod);
+
+        chooseLoginMethod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=   new Intent(LoginActivity.this, OAuthActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
