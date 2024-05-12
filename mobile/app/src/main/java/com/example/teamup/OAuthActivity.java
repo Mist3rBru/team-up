1bfabc48f8fd7036216c2298b73d5d3ed2c5e714
Bruno Rocha <brunorocha2674@gmail.com>
2024-05-11 21:41:12 -0300

    feat: add back button to oauth and sign up views

--- mobile/app/src/main/java/com/example/teamup/OAuthActivity.java
+++ mobile/app/src/main/java/com/example/teamup/OAuthActivity.java
@@ -1,6 +1,9 @@
 package com.example.teamup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,15 @@ public class OAuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oauth);
+
+        ImageView back_icon = findViewById(R.id.back_icon);
+
+        back_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OAuthActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- mobile/app/src/main/java/com/example/teamup/SignUpActivity.java
+++ mobile/app/src/main/java/com/example/teamup/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.teamup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ImageView back_icon = findViewById(R.id.back_icon);
+
+        back_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
