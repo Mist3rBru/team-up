c1c01870236b4ebe48458371f93351d55e1ac579
kaudc <kau.dalla@gmail.com>
2024-05-13 19:07:26 -0300

    finally game center activity

--- mobile/app/src/main/java/com/example/teamup/GameNetworkActivity.java
+++ mobile/app/src/main/java/com/example/teamup/GameNetworkActivity.java
@@ -1,9 +1,6 @@
 package com.example.teamup;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class GameCenterActivity extends AppCompatActivity {
+public class GameNetworkActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_game_center);
-
-        ImageView imageView = findViewById(R.id.view6);
-
+        setContentView(R.layout.activity_game_network);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=   new Intent(GameCenterActivity.this, GameNetworkActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
     }
 }
